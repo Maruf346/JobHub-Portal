3fa528cab5a681caa375bc0d15a3606cd7011be9
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-01 20:30:52 +0600

    feat: add ViewAllJobs component and update JobListings to display recent jobs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import HomeCards from './components/HomeCards.jsx'
 import JobListings from './components/JobListings.jsx'
+import ViewAllJobs from './components/ViewAllJobs.jsx'
 
 const App = () => {
   return (
@@ -10,14 +11,7 @@ const App = () => {
       <Hero title="Welcome to JobHub" subtitle="Find Your Dream Job Today" />
       <HomeCards />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-        >
-      </section>
+      <ViewAllJobs />
     </>
   )
 }
