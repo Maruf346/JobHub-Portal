87d98f7102686d0792a99c8f66c9cc471a3788ad
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-03 23:12:11 +0600

    feat: update API endpoints in JobListings component and configure Vite proxy for API requests

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,15 @@ export default defineConfig({
   plugins: [
     react(),
   ],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    },
+  },
 })
