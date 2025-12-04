d346bbdeb6c67142acf1af0fd2c78ada1e15244f
Maruf Hossain <maruf.bshs@gmail.com>
2025-12-04 17:40:06 +0600

    feat: enhance JobPage layout with detailed job information and navigation links

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,14 +1,98 @@
 import { useParams, useLoaderData } from "react-router-dom";
+import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const JobPage = () => {
     const { id } = useParams();
     const job = useLoaderData();
 
     return (
-        <div>
-            <h1>{job.title}</h1>
-            <p>{job.description}</p>
-        </div>
+        <>
+            <section>
+                <div className="container m-auto py-6 px-6">
+                    <Link
+                        to="/jobs/"
+                        className="text-indigo-500 hover:text-indigo-600 flex items-center"
+                    >
+                        <FaArrowLeft className="mr-2" /> Back to Job Listings
+                    </Link>
+                </div>
+            </section>
+
+            <section className="bg-indigo-50">
+                <div className="container m-auto py-10 px-6">
+                    <div className="grid grid-cols-1 md:grid-cols-12 w-full gap-6">
+                        <main className="md:col-span-8">
+                            <div
+                                className="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
+                            >
+                                <div class="text-gray-500 mb-4">{job.type}</div>
+                                <h1 class="text-3xl font-bold mb-4">
+                                    {job.title}
+                                </h1>
+                                <div
+                                    class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
+                                >
+                                    <FaMapMarker className="inline text-orange-700 mb-1 mr-2" />
+                                    <p className="text-orange-700">{job.location}</p>
+                                </div>
+                            </div>
+
+                            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                                <h3 class="text-indigo-800 text-lg font-bold mb-6">
+                                    Job Description
+                                </h3>
+
+                                <p class="mb-4">
+                                    {job.description}
+                                </p>
+
+                                <h3 class="text-indigo-800 text-lg font-bold mb-2">Salary</h3>
+
+                                <p class="mb-4">{job.salary}</p>
+                            </div>
+                        </main>
+
+                        <aside className="md:col-span-4">
+                            <div className="bg-white p-6 rounded-lg shadow-md">
+                                <h3 class="text-xl font-bold mb-6">Company Info</h3>
+
+                                <h2 class="text-2xl">{job.company.name}</h2>
+
+                                <p class="my-2">
+                                    {job.company.description}
+                                </p>
+
+                                <hr className="my-4" />
+
+                                <h3 class="text-xl">Contact Email:</h3>
+
+                                <p className="my-2 bg-indigo-100 p-2 font-bold">
+                                    {job.company.contactEmail}
+                                </p>
+
+                                <h3 class="text-xl">Contact Phone:</h3>
+
+                                <p className="my-2 bg-indigo-100 p-2 font-bold">{job.company.contactPhone}</p>
+                            </div>
+
+                            <div className="bg-white p-6 rounded-lg shadow-md mt-6">
+                                <h3 class="text-xl font-bold mb-6">Manage Job</h3>
+                                <Link
+                                    to={`/jobs/edit/${job.id}`}
+                                    className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                                >Edit Job</Link>
+                                <button
+                                    className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                                >
+                                    Delete Job
+                                </button>
+                            </div>
+                        </aside>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
 
